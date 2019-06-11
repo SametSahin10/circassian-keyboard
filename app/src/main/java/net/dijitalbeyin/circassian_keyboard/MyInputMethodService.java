c42508b497b29d2c5760e2bb7fdcceb30e6eb068
Samet Sahin <a.sahin.ual@gmail.com>
2019-06-11 22:51:33 +0300

    Can use the keyboard to type

--- app/src/main/java/net/dijitalbeyin/circassian_keyboard/MyInputMethodService.java
+++ app/src/main/java/net/dijitalbeyin/circassian_keyboard/MyInputMethodService.java
@@ -1,9 +1,28 @@
 package net.dijitalbeyin.circassian_keyboard;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
 
 public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    private KeyboardView keyboardView;
+    private Keyboard keyboard;
+    private boolean isCaps = false;
+
+    @Override
+    public View onCreateInputView() {
+        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        keyboard = new Keyboard(this, R.xml.keys_layout);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
     @Override
     public void onPress(int primaryCode) {
 
@@ -16,7 +35,34 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
 
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
-
+        InputConnection inputConnection = getCurrentInputConnection();
+        if (inputConnection != null) {
+            playSound(primaryCode);
+            switch (primaryCode) {
+                case Keyboard.KEYCODE_DELETE:
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+                    break;
+                case Keyboard.KEYCODE_SHIFT:
+                    isCaps = !isCaps;
+                    keyboard.setShifted(isCaps);
+                    keyboardView.invalidateAllKeys();
+                    break;
+                case Keyboard.KEYCODE_DONE:
+                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                    break;
+                default:
+                    char code = (char) primaryCode;
+                    if (Character.isLetter(code) && isCaps) {
+                        code = Character.toUpperCase(code);
+                    }
+                    inputConnection.commitText(String.valueOf(code), 1);
+            }
+        }
     }
 
     @Override
@@ -42,5 +88,23 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
     @Override
     public void swipeUp() {
 
+    }
+
+    private void playSound(int primaryCode) {
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (primaryCode) {
+            case 32:
+                audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default:
+                audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
     }
 }
