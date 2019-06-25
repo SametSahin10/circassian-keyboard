870d1f943375aa02bdedfb0bae1eeb7c4e038738
Samet Sahin <a.sahin.ual@gmail.com>
2019-06-25 15:57:51 +0300

    Added logo and settings button to MainActivity

--- app/src/main/java/net/dijitalbeyin/circassian_keyboard/MainActivity.java
+++ app/src/main/java/net/dijitalbeyin/circassian_keyboard/MainActivity.java
@@ -1,13 +1,26 @@
 package net.dijitalbeyin.circassian_keyboard;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_goToSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_goToSettings = findViewById(R.id.btn_goToSettings);
+        btn_goToSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Settings.ACTION_SETTINGS), 0);
+            }
+        });
     }
 }
